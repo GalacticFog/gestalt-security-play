04a31fae212f66c2af1c3be5507e564e3b6e89bc
Christopher Baker <cgbaker@gmail.com>
2015-05-07 14:50:12 -0400

    added WWW-Authenticate header to NotAuthorized response

--- src/main/scala/com/galacticfog/gestalt/security/play/silhouette/utils/GlobalWithInjection.scala
+++ src/main/scala/com/galacticfog/gestalt/security/play/silhouette/utils/GlobalWithInjection.scala
@@ -3,6 +3,12 @@ package com.galacticfog.gestalt.security.play.silhouette.utils
 import com.google.inject.Guice
 import com.mohiva.play.silhouette.api.{Logger, SecuredSettings}
 import play.api.GlobalSettings
+import play.api.i18n.Lang
+import play.api.mvc.{Result, RequestHeader}
+import play.api.mvc.Results._
+import scala.concurrent.ExecutionContext.Implicits.global
+
+import scala.concurrent.Future
 
 /**
  * The global configuration.
@@ -23,5 +29,18 @@ trait GlobalWithInjection extends GlobalSettings with Logger with SecuredSetting
    * @throws Exception if the controller couldn't be instantiated.
    */
   override def getControllerInstance[A](controllerClass: Class[A]) = injector.getInstance(controllerClass)
+
+  /**
+   * Called when a user is not authenticated.
+   *
+   * As defined by RFC 2616, the status code of the response should be 401 Unauthorized.
+   *
+   * @param request The request header.
+   * @param lang The currently selected language.
+   * @return The result to send to the client.
+   */
+  override def onNotAuthenticated(request: RequestHeader, lang: Lang): Option[Future[Result]] = Some(Future{
+    Unauthorized("").withHeaders("WWW-Authenticate" -> "basic")
+  })
 
 }
