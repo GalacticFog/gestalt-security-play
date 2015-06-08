2ae8c2e624721e649ec86d1b2b7edcfc66715ad0
Christopher Baker <cgbaker@gmail.com>
2015-06-08 11:30:51 -0400

    added json serialization for AuthAccount

--- src/main/scala/com/galacticfog/gestalt/security/play/silhouette/utils/JsonImports.scala
+++ src/main/scala/com/galacticfog/gestalt/security/play/silhouette/utils/JsonImports.scala
@@ -1,6 +1,7 @@
 package com.galacticfog.gestalt.security.play.silhouette.utils
 
 import com.galacticfog.gestalt.security.play.silhouette.AuthAccount
+import com.galacticfog.gestalt.security.api.json.JsonImports.{acctFormat,grantFormat}
 import play.api.libs.json.Json
 
 object JsonImports {
