59659d1a67e51626d3495e387febbb76835f6265
Christopher Baker <cgbaker@gmail.com>
2015-06-27 03:45:23 -0400

    added gestalt-io context debugging output to GlobalMeta

--- src/main/scala/com/galacticfog/gestalt/meta/play/utils/GlobalMeta.scala
+++ src/main/scala/com/galacticfog/gestalt/meta/play/utils/GlobalMeta.scala
@@ -1,9 +1,13 @@
 package com.galacticfog.gestalt.meta.play.utils
 
 import com.galacticfog.gestalt.Gestalt
+import com.galacticfog.gestalt.io.GestaltConfig.GestaltContext
+import play.api.Logger
+import play.api.libs.json.Json
 
 trait GlobalMeta {
   this : com.galacticfog.gestalt.meta.play.utils.GlobalMeta with play.api.GlobalSettings =>
 
   val meta: Gestalt = new Gestalt()
+  meta.local.context.foreach { (c: GestaltContext) => Logger.info(Json.prettyPrint(c.toJson)) }
 }
