4ec006690a2fffde7c761e78bab92b32d773faa3
Christopher Baker <cgbaker@gmail.com>
2017-01-06 16:01:41 -0500

    added example app to CI

--- examples/gestalt-framework-auth/test-with-docker.sh
+++ examples/gestalt-framework-auth/test-with-docker.sh
@@ -10,20 +10,24 @@ set -o nounset
 set -o pipefail
 # set -x
 
-# it's easier to remove it and then start a new one than to try to restart it if it exists with fallback on creation
-
 echo Starting database in docker
 docker pull postgres:9.4
-db=$(docker run -P -d -e POSTGRES_DB=$DBNAME -e POSTGRES_USER=$DBUSER -e POSTGRES_PASSWORD=$DBPASS postgres:9.4)
+dbcon=$(docker run -P -d -e POSTGRES_DB=$DBNAME -e POSTGRES_USER=$DBUSER -e POSTGRES_PASSWORD=$DBPASS postgres:9.4)
 
-DOCKERIP=$(docker inspect $db | jq -r '.[0].NetworkSettings.Ports."5432/tcp"[0].HostIp')
+# this is the IP that we'll use to talk to the docker host
+DOCKERIP=$(docker inspect $dbcon | jq -r '.[0].NetworkSettings.Ports."5432/tcp"[0].HostIp')
 if [ "$DOCKERIP" == "0.0.0.0" ]; then 
-  DOCKERIP="localhost"
+  if [ -z ${DOCKER_PORT_2375_TCP+x} ]; then 
+    DOCKERIP="localhost"
+  else
+    # gitlab ci links docker:dind container to ours, with hostname docker
+    DOCKERIP="docker"
+  fi
 fi
-DBPORT=$(docker inspect $db | jq -r '.[0].NetworkSettings.Ports."5432/tcp"[0].HostPort')
-DBHOST=$(docker inspect $db | jq -r '.[0].NetworkSettings.IPAddress')
+DBPORT=$(docker inspect $dbcon | jq -r '.[0].NetworkSettings.Ports."5432/tcp"[0].HostPort')
+DBHOST=$(docker inspect $dbcon | jq -r '.[0].NetworkSettings.IPAddress')
 echo "
-DB running at $DOCKERIP:$DBPORT / $DBHOST:5432
+Database running at $DOCKERIP:$DBPORT / $DBHOST:5432
 "
 
 export DATABASE_HOSTNAME=$DBHOST
@@ -31,29 +35,28 @@ export DATABASE_NAME=$DBNAME
 export DATABASE_PORT=5432
 export DATABASE_USERNAME=$DBUSER
 export DATABASE_PASSWORD=$DBPASS
-
 echo "
 Running gestalt-security
 "
 docker pull $SECURITYIMG
-security=$(docker run -P -d -e DATABASE_HOSTNAME -e DATABASE_NAME -e DATABASE_PORT -e DATABASE_USERNAME -e DATABASE_PASSWORD $SECURITYIMG)
+scon=$(docker run -P -d -e DATABASE_HOSTNAME -e DATABASE_NAME -e DATABASE_PORT -e DATABASE_USERNAME -e DATABASE_PASSWORD $SECURITYIMG)
 
-SECPORT=$(docker inspect $security | jq -r '.[0].NetworkSettings.Ports."9000/tcp"[0].HostPort')
-SECHOST=$(docker inspect $security | jq -r '.[0].NetworkSettings.IPAddress')
+SECPORT=$(docker inspect $scon | jq -r '.[0].NetworkSettings.Ports."9000/tcp"[0].HostPort')
+SECHOST=$(docker inspect $scon | jq -r '.[0].NetworkSettings.IPAddress')
 echo "
 Security running at $DOCKERIP:$SECPORT / $SECHOST:9000
 "
 
 cleanup_docker() {
 echo ""
-echo Stopping db container
-echo Stopped $(docker stop $db)
-echo Removing db container
-echo Removed $(docker rm $db)
+echo Stopping database container
+echo Stopped $(docker stop $dbcon)
+echo Removing database container
+echo Removed $(docker rm $dbcon)
 echo Stopping security container
-echo Stopped $(docker stop $security)
+echo Stopped $(docker stop $scon)
 echo Removing security container
-echo Removed $(docker rm $security)
+echo Removed $(docker rm $scon)
 
 echo Killing play app
 if [ -e ./target/universal/stage/RUNNING_PID ]; then
@@ -71,8 +74,7 @@ echo "
 Initializing gestalt-security
 "
 sleep 10
-
-init=$(http $DOCKERIP:$SECPORT/init username=testuser password=testpassword)
+init=$(http --ignore-stdin $DOCKERIP:$SECPORT/init username=testuser password=testpassword)
 
 export GESTALT_SECURITY_HOSTNAME=$DOCKERIP
 export GESTALT_SECURITY_PORT=$SECPORT
@@ -84,17 +86,20 @@ Gestalt security environment variables for app:"
 env | grep GESTALT
 
 echo "
+Building/testing app
+"
+sbt clean update test stage
+
+echo "
 Running example app in the background
 "
-
-sbt clean update test stage
 ./target/universal/stage/bin/gestalt-framework-auth &
 
 sleep 10
 
 echo "
 Auth attempt with no credentials:"
-bad=$(http localhost:9000/ Authorization: || true)
+bad=$(http --ignore-stdin localhost:9000/ Authorization: || true)
 if [[ `echo $bad | jq -r '.code'` -ne 401 ]]; then 
   echo "***** DID NOT GET EXPECTED 401"
   exit 1
@@ -104,7 +109,7 @@ fi
 
 echo "
 Auth attempt with bad credentials:"
-bad=$(http -a BAD:CREDS localhost:9000/ || true)
+bad=$(http --ignore-stdin -a BAD:CREDS localhost:9000/ || true)
 if [[ `echo $bad | jq -r '.code'` -ne 401 ]]; then 
   echo "***** DID NOT GET EXPECTED 401"
   exit 1
@@ -114,10 +119,13 @@ fi
 
 echo "
 Auth attempt with good credentials:"
-http --check-status -a "$GESTALT_SECURITY_KEY":"$GESTALT_SECURITY_SECRET" localhost:9000/
+good=$(http --ignore-stdin --check-status -a "$GESTALT_SECURITY_KEY":"$GESTALT_SECURITY_SECRET" localhost:9000/
+echo "Got response:
+$good
+"
 
 echo "
-Manual tests passed.
+TESTS PASSED.
 "
 
 exit 0
